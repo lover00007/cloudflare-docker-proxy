972cd50fc5120fb8d04012d3f84114f095df352e
lover00007 <60789733+lover00007@users.noreply.github.com>
2024-06-19 13:28:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.122899.xyz": dockerHub,
+  "quay.122899.xyz": "https://quay.io",
+  "gcr.122899.xyz": "https://gcr.io",
+  "k8s-gcr.122899.xyz": "https://k8s.gcr.io",
+  "k8s.122899.xyz": "https://registry.k8s.io",
+  "ghcr.122899.xyz": "https://ghcr.io",
+  "cloudsmith.122899.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.122899.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
